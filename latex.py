a903bd07a6219455ab203629cd346b9edccdb937
Jeffrey M Brown <brown.jeffreym@gmail.com>
2013-07-09 22:32:26 -0700

    Added more functions for conversion of song elements into latex code

--- latex.py
+++ latex.py
@@ -29,9 +29,38 @@ def paren_to_latex(paren):
     """Returns latex command for the song.Parentheses class"""
     return latex_command("emph", "({})".format(paren.text))
 
+
+
+def to_latex(song_element):
+    """Function that calls appropriate latex conversion function for given song element"""
+    if isinstance(song_element, song.Chord):
+        return chord_to_latex(song_element)
+    elif isinstance(song_element, song.Text):
+        return text_to_latex(song_element)
+    elif isinstance(song_element, song.Chordline):
+        return chordline_to_latex(song_element)
+    elif isinstance(song_element, song.Parentheses):
+        return paren_to_latex(song_element)
+    else:
+        raise ValueError("Unknown song element: {}".format(type(song_element)))
+
+
 def stanza_to_latex(stanza):
-    """Returns latex command for the song.Stanza class"""
+    """Returns latex code for the song.Stanza class"""
+    formatted_lines = []
+    for line in stanza.lines:
+        formatted_lines.append(''.join([to_latex(element) for element in line]))
+    text = '\\\\\n'.join(formatted_lines) # add latex newline \\ and ascii newline
+    return latex_command(stanza.type, text)
+
+def song_to_latex(song):
+    """Returns latex code for the song.Song class"""
     pass
+    
+def write(text):
+    with open('test', 'w') as f:
+        f.write(text)
 
 if __name__ == '__main__':
     s = song.Song('testsong')
+    c = s.attributes['chorus']
