70f99dd6c17b95dd5a92625b41a1bc835ca96386
Jeffrey M Brown <brown.jeffreym@gmail.com>
2013-07-09 21:34:24 -0700

    Added comments

--- latex.py
+++ latex.py
@@ -7,23 +7,31 @@ stanzaList = ["verse", "chorus", "prechorus", "bridge", "intro", "outro"]
 
 
 def latex_command(command, arg):
+    """Returns a latex command with a single argument"""
     return "\{}{{{}}}".format(command, arg)
 
 def chord_to_latex(chord):
+    """Returns latex command for the song.Chord class"""
     chord = chord.text.replace('#', '\#') # latex requires backslash
     return latex_command("chord", chord)
 
 def chordline_to_latex(chordline):
+    """Returns latex command for the song.Chordline class"""
     return latex_command("chordline", chordline.text)
 
 def text_to_latex(text):
+    """Returns latex command for the song.Text class"""
     text = text.text.replace('   ', latex_command('hspace', '3mm'))
     text = text.replace('&', '\&') # latex requires backslash
     return text
 
 def paren_to_latex(paren):
+    """Returns latex command for the song.Parentheses class"""
     return latex_command("emph", "({})".format(paren.text))
 
+def stanza_to_latex(stanza):
+    """Returns latex command for the song.Stanza class"""
+    pass
 
 if __name__ == '__main__':
-    print(latex_command("emph", "hello"))
+    s = song.Song('testsong')
